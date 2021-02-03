49d5eee8375a5d335124ac97f96e08680412c464
Shawn Roller <sroller@techstyle.com>
2021-02-03 13:16:58 -0500

    Initial commit.

---
+++ AzureADDemo/Models/ADConfig.swift
@@ -0,0 +1,14 @@
+//
+//  ADConfig.swift
+//  AzureADDemo
+//
+//  Created by Shawn Roller on 2/3/21.
+//
+
+import Foundation
+
+struct ADConfig {
+    let clientID: String
+    let graphEndpoint: String
+    let authority: String
+}
